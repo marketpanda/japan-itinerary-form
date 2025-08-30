e7f9c25f0ef9b690278202b8d08cdcb61ef9f976
marketpanda <jrdollesin@gmail.com>
2025-08-30 10:01:13 +0800

    remove any es-lint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,10 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "import/no-anonymous-default-export": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
   },
 ];
 
